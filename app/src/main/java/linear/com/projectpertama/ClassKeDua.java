8dba7c27ae261bbe270df085abf4718b1550254b
iChwn <151610209@smkassalaambandung.sch.id>
2017-07-27 11:44:27 +0400

    baru sampai menampilkan gambar

---
+++ app/src/main/java/linear/com/projectpertama/ClassKeDua.java
@@ -0,0 +1,14 @@
+package linear.com.projectpertama;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ClassKeDua extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_ke_dua);
+    }
+
+}
---
+++ app/src/main/java/linear/com/projectpertama/RelativeLayoutSederhana.java
@@ -0,0 +1,14 @@
+package linear.com.projectpertama;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RelativeLayoutSederhana extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_sederhana);
+    }
+
+}
